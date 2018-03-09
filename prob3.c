21f3c608c5cc3e38facf9e9c57e3c6ed9bdd9bac
Kieran Halliday <kluhini96@users.noreply.github.com>
2018-03-09 15:20:42 -0800

    Update prob3.c

--- prob3.c
+++ prob3.c
@@ -1,44 +1,33 @@
 #include <stdio.h>
 
+//The prime factors of 13195 are 5, 7, 13 and 29.
+//What is the largest prime factor of the number 600851475143 ?
+
 int main (void){
+	const long num = 600851475143;
+	long counter=num/2+1;
 
-long num=600851475143;
-long counter=num/2+1;
+	while(--counter>0){
 
-printf("calculating\n");
+		if(num%counter==0 && check_prime(counter)==1)
+			break;
+	}
 
-while(--counter>0){
+	printf("%ld\n",counter);
 
-	if(num%counter==0 && check_prime(counter)==1)
-		break;
-}
-
-printf("%ld\n",counter);
-
-
-
-return 0;
+	return 0;
 }
 
 
 int check_prime(long num){
+	long i;
+	
+	if (num == 2) return 1;
+	else if (num % 2 == 0) return 0;
 
-//	long i;
-//	for(i=1;i<=num/2;i++){
-//		if(num%i==0)
-//		return 0;
-//	}
+	for (i = 3; i <= num/2; i += 2) {
+    		if (num%i==0) return 0;
+	}
 
-//	return 1;
-
-if (num == 2) return 1;
-else if (num % 2 == 0) return 0;
-long i;
-for (i = 3; i <= num/2; i += 2) {
-    if (num%i==0) return 0;
-}
-
-return 1;
-
-
+	return 1;
 }
