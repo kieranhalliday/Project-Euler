114813eb0be324857cc2477912020f23bf2a7056
Kieran Halliday <kluhini96@users.noreply.github.com>
2018-03-09 15:15:25 -0800

    Update prob1.c

--- prob1.c
+++ prob1.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
+//If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+//Find the sum of all the multiples of 3 or 5 below 1000.
 int main (void){
 	int max=1000;
 	int num=1;
 	int sum=0;
 	while(num<1000){
-
 		if(num%3==0){
-		sum=sum+num;
+			sum=sum+num;
 		}
 		else if(num%5==0){
-		sum=sum+num;
+			sum=sum+num;
 		}
 	num++;
 }
 
-printf("%d\n",sum);
+	printf("%d\n",sum);
 }
