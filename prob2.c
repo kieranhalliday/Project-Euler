56987c55f425ea26b603570aef4735ae0a71ebf4
Kieran Halliday <kluhini96@users.noreply.github.com>
2018-03-09 15:17:11 -0800

    Update prob2.c

--- prob2.c
+++ prob2.c
@@ -1,20 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// By considering the terms in the Fibonacci sequence whose values do not exceed four million, find the sum of the even-valued terms.
+
 int main (void){
+	const int MAX = 4000000
+	int low=1, high=2,sum=0,temp;
 
-int low=1;
-int high=2;
-int sum=0;
-int temp;
-
-while(low<4000000){
-	if(low%2==0){
-		sum+=low;
+	while(low < MAX){
+		if(low % 2 == 0){
+			sum+=low;
+		}
+		temp=high;
+		high+=low;
+		low=temp;
 	}
-	temp=high;
-	high+=low;
-	low=temp;
-}
-printf("%d\n",sum);
+	printf("%d\n",sum);
 }
