33f584724b3ead35170c6cf518365cb209961fe4
Kieran Halliday <kluhini96@users.noreply.github.com>
2018-03-09 15:22:40 -0800

    Update prob6.c

--- prob6.c
+++ prob6.c
@@ -1,36 +1,36 @@
 #include <stdio.h>
 
+//Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+
 int main (void){
 
-int difference=s_of_squares(100)-s_of_sums(100);
+	int difference=s_of_squares(100)-s_of_sums(100);
 
-printf("%d\n",difference);
+	printf("%d\n",difference);
 
-return 0;
-
+	return 0;
 }
 
 
 
 int s_of_squares(int max){
-int counter=1;
-int sum=0;
+	int counter=1;
+	int sum=0;
 
-for(counter;counter<=max;counter++){
-	sum+=counter*counter;
-}
+	for(counter;counter<=max;counter++){
+		sum+=counter*counter;
+	}
 
-return sum;
+	return sum;
 }
 
 int s_of_sums(int max){
-int counter=1;
-int sum=0;
+	int counter=1;
+	int sum=0;
 
-for(counter;counter<=max;counter++){
-	sum+=counter;
-}
-
-sum=sum*sum;
-return sum;
+	for(counter;counter<=max;counter++){
+		sum+=counter;
+	}
+	sum=sum*sum;
+	return sum;
 }
