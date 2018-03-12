4c47516a80f9cd9ec571b9ca0696a59bcecf8b4b
Kieran Halliday <kluhini96@users.noreply.github.com>
2018-03-12 12:09:40 -0700

    Working version of problem 22

---
+++ prob22.cpp
@@ -0,0 +1,104 @@
+//
+//  main.cpp
+//  Euler
+//
+//  Created by Halliday, Kieran on 2018-03-06.
+//  Copyright © 2018 Halliday, Kieran. All rights reserved.
+//
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+// Get score of a single name
+int getScore(string name){
+    int score = 0;
+    
+    for(int i=0;i<name.size();i++){
+        // Char must be a capital letter in ASCII representation
+        if((int)name[i] >=65 && (int)name[i]<=90){
+            score+=((int)name[i])-64;
+        }
+    }
+    return score;
+}
+
+// Used in quicksort
+int myPartition(vector<string> &names, int lo, int hi){
+    string pivot = names[lo];
+    int i = lo;
+    int j = hi;
+    
+    while(true){
+        while(names[i].compare(pivot)<0){
+            i++;
+        }
+        while(names[j].compare(pivot)>0){
+            j--;
+        }
+        if(i>=j){
+            return j;
+        }
+        
+        // Swap names[i] and names[j]
+        string temp = names[i];
+        names[i] = names[j];
+        names[j] = temp;
+    }
+}
+
+void quickSort(vector<string> &names, int lo, int hi){
+    // Generate random pivot
+    if(lo<hi){
+        int p = myPartition(names, lo, hi);
+    
+        // Sort lower side
+        quickSort(names,lo,p);
+        // Sort upper side
+        quickSort(names,p+1,hi);
+    }
+}
+
+void p22(){
+    vector<string> names;
+    ifstream inputFileName;
+    unsigned long score = 0;
+    
+    inputFileName.open("p022_names.txt");
+    
+    if(inputFileName.is_open()){
+        string text;
+        string delimiter = ",";
+        inputFileName >> text;
+        
+        // Iterate through while names still exist in the file
+        string::iterator it = text.begin();
+        string name;
+        while(it != text.end()){
+            name += *it;
+            it++;
+            if(name.find(delimiter) != string::npos){
+                names.push_back(name);
+                name = "";
+            }
+        }
+        names.push_back(name);
+        
+        // Sort names in alphabetical order
+        quickSort(names,0,names.size());
+        
+        // Count score
+        for(int i=0;i<=names.size();i++){
+            score += ((i) * getScore(names[i]));
+        }
+        cout << score << endl;
+        inputFileName.close();
+    }
+}
+int main(int argc, const char * argv[]) {
+    p22();
+    return 0;
+}
